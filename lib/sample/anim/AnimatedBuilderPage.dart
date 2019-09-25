12a8f56cae0261d03fabcac91b68516ae1595ee5
Air <xkrossiapd163@163.com>
2019-09-25 18:10:06 +0800

    补充：AnimatedBuilder和AnimatedContainer的效果展示，以及区分

--- lib/sample/anim/AnimatedBuilderPage.dart
+++ lib/sample/anim/AnimatedBuilderPage.dart
@@ -1,21 +1,123 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 import 'package:flutter_app_sample/ui/base/AppCommonStatefulPage.dart';
 
 ///
 /// AnimatedBuilderPage
 ///
-class AnimatedBuilderPage extends AppCommonStatefulPage {
-  String _titleName = "AnimatedBuilder";
+///
+class AnimatedBuilderPage extends StatefulWidget {
+  @override
+  _SpinnerState createState() => _SpinnerState();
+}
+
+class _SpinnerState extends State<AnimatedBuilderPage>
+    with SingleTickerProviderStateMixin {
+  String _titleName = "AnimatedBuilderPage";
+
+  bool _selected = false;
+
+  AnimationController _controller;
 
   @override
-  Config createConfig() {
-    return Config(titleName: "$_titleName");
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat();
   }
 
   @override
-  Widget createWidget() {
-    return Center(
-      child: Text("TODO"),
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("$_titleName"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text(
+                "使用AnimatedBuilder为widget装饰动画效果，这是相对AnimatedContainer更强大的扩展哦"),
+          ),
+          Center(
+            child: AnimatedBuilder(
+              animation: _controller,
+              child:
+                  Container(width: 200.0, height: 200.0, color: Colors.green),
+              builder: (BuildContext context, Widget child) {
+                return Transform.rotate(
+                  angle: _controller.value * 2.0 * math.pi,
+                  child: Container(
+                    width: 200.0,
+                    height: 200.0,
+                    color: ColorTween(begin: Colors.red, end: Colors.blue)
+                        .transform(_controller.value),
+                  ),
+                );
+              },
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _selected = !_selected;
+              });
+            },
+            child: AnimatedContainer(
+              width: 200,
+              height: 200,
+              duration: Duration(seconds: 3),
+              curve: Curves.fastOutSlowIn,
+              child: Container(
+                child: Text(
+                  "Style",
+                  style: TextStyle(
+                    color: _selected ? Colors.red : Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          AnimatedBuilder(
+              animation: _controller,
+              child: Text(
+                "AnimatedBuilder-Color",
+                style: TextStyle(
+                    color: ColorTween(begin: Colors.red, end: Colors.blue)
+                        .transform(_controller.value)),
+              ),
+              builder: (BuildContext context, Widget widget) {
+                ///
+                /// 此函数参数widget是AnimatedBuilder的child
+                /// return 返回的widget会替代AnimatedBuilder的child
+                ///
+                return Column(
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      height: 200,
+                      color: ColorTween(begin: Colors.red, end: Colors.blue)
+                          .transform(_controller.value),
+                    ),
+                    Text(
+                      "AnimatedBuilder-Color , value = ${(_controller.value * 10).toInt() / 10}",
+                      style: TextStyle(
+                          color: ColorTween(begin: Colors.red, end: Colors.blue)
+                              .transform(_controller.value)),
+                    )
+                  ],
+                );
+              })
+        ],
+      ),
     );
   }
 }
--- lib/sample/anim/AnimatedContainerPage.dart
+++ lib/sample/anim/AnimatedContainerPage.dart
@@ -25,37 +25,65 @@ class AnimatedContainerPage extends AppCommonStatefulPage {
           selected = !selected;
         });
       },
-      child: Center(
-        child: AnimatedContainer(
-          width: selected ? 300.0 : 200.0,
-          height: selected ? 200.0 : 300.0,
-          color: selected ? Colors.red : Colors.blue,
-          alignment:
-              selected ? Alignment.center : AlignmentDirectional.topCenter,
-          duration: Duration(seconds: durationUnitSecond),
-          curve: Curves.fastOutSlowIn,
-          child: Column(
-            children: <Widget>[
-              FlutterLogo(size: 75),
-              Text(
-                "Flutter's AnimatedContainer!",
-                style: TextStyle(color: Colors.white),
+      child: Column(
+        children: <Widget>[
+          ///
+          /// AnimatedContainer演示效果
+          ///
+          Center(
+            child: AnimatedContainer(
+              width: selected ? 300.0 : 200.0,
+              height: selected ? 200.0 : 300.0,
+              color: selected ? Colors.red : Colors.blue,
+              alignment:
+                  selected ? Alignment.center : AlignmentDirectional.topCenter,
+              duration: Duration(seconds: durationUnitSecond),
+              curve: Curves.fastOutSlowIn,
+              child: Column(
+                children: <Widget>[
+                  FlutterLogo(size: 75),
+                  Text(
+                    "Flutter's AnimatedContainer!",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    "click the rect to show anim!",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  RaisedButton(
+                    animationDuration: Duration(seconds: durationUnitSecond),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(selected ? 15 : 0),
+                    ),
+                    child: Text("AnimatedContainerPage"),
+                    onPressed: () {},
+                  ),
+                ],
               ),
-              Text(
-                "click the rect to show anim!",
-                style: TextStyle(color: Colors.white),
-              ),
-              RaisedButton(
-                animationDuration: Duration(seconds: durationUnitSecond),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(selected ? 15 : 0),
-                ),
-                child: Text("AnimatedContainerPage"),
-                onPressed: () {},
-              ),
-            ],
+            ),
           ),
-        ),
+
+          ///
+          /// 以下说明AnimatedContainer的限制场景
+          ///
+          AnimatedContainer(
+            width: 200.0,
+            height: 200.0,
+            color: selected ? Colors.red : Colors.blue,
+            duration: Duration(seconds: durationUnitSecond),
+            curve: Curves.fastOutSlowIn,
+            child: Container(
+              width: 200,
+              height: 200,
+              child: Text(
+                "请注意这段文字：AnimatedContainer只能为AnimatedContainer自身设置动画（自身不包括AnimatedContainer的child,以及更深层次的child...\n这是AnimatedContainer的限制和短处）",
+                style: TextStyle(
+                  color: selected ? Colors.white : Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
