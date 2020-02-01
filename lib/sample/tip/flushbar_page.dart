1e93b38a9809f230102489affef89388236bc025
Air <xkrossiapd163@163.com>
2020-02-01 18:36:25 +0800

    Add:'flutter tip message widget' model

--- lib/sample/tip/flushbar_page.dart
+++ lib/sample/tip/flushbar_page.dart
@@ -22,7 +22,7 @@ class _FlushBarState extends State<FlushBarPage> {
   int _tipSecondDuration = 0;
 
   List<bool> _toggleSelected = [true, false, false, false];
-  List<String> _toggleLabels = ["info", "warnning", "error", "completed"];
+  List<String> _toggleLabels = ["info", "warn", "error", "complete"];
   List<Color> _tipStyleColors = [
     Colors.blue[100],
     Colors.yellow[300],
@@ -56,7 +56,7 @@ class _FlushBarState extends State<FlushBarPage> {
       flushbarPosition: FlushbarPosition.TOP,
       shouldIconPulse: _tipIconPulse,
       showProgressIndicator: _tipShowProgress,
-      flushbarStyle: FlushbarStyle.FLOATING,
+      flushbarStyle: FlushbarStyle.GROUNDED,
       backgroundColor: _tipStyleColor,
       borderColor: _tipStyleColor,
       progressIndicatorBackgroundColor: _tipIndicatorColor,
@@ -99,148 +99,150 @@ class _FlushBarState extends State<FlushBarPage> {
       appBar: AppBar(
         title: Text("Tip msg"),
       ),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Container(
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          "preview",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            "preview",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
+                          height: 100,
+                          alignment: Alignment.center,
                         ),
-                        height: 100,
-                        alignment: Alignment.center,
-                      ),
-                    ],
-                  ),
-                  _getFlushBar(),
-                ],
+                      ],
+                    ),
+                    _getFlushBar(),
+                  ],
+                ),
               ),
-            ),
-            Divider(
-              color: Colors.grey,
-              thickness: 1,
-            ),
-            _getStatusToggle(),
-            Divider(
-              color: Colors.grey,
-              thickness: 1,
-            ),
-            TextField(
-              obscureText: false,
-              onChanged: (title) {
-                setState(() {
-                  _tipTitle = title;
-                });
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'title',
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
               ),
-            ),
-            Divider(),
-            TextField(
-              obscureText: false,
-              onChanged: (content) {
-                setState(() {
-                  _tipMessage = content;
-                });
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'content',
+              _getStatusToggle(),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
               ),
-            ),
-            ListTile(
-              title: Text('Icon show'),
-              trailing: Switch(
-                value: _tipIconShow,
-                onChanged: (bool value) {
+              TextField(
+                obscureText: false,
+                onChanged: (title) {
                   setState(() {
-                    _tipIconShow = value;
+                    _tipTitle = title;
+                  });
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'title',
+                ),
+              ),
+              Divider(),
+              TextField(
+                obscureText: false,
+                onChanged: (content) {
+                  setState(() {
+                    _tipMessage = content;
+                  });
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'content',
+                ),
+              ),
+              ListTile(
+                title: Text('Icon show'),
+                trailing: Switch(
+                  value: _tipIconShow,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _tipIconShow = value;
+                    });
+                  },
+                ),
+                onTap: () {
+                  setState(() {
+                    _tipIconShow = !_tipIconShow;
                   });
                 },
               ),
-              onTap: () {
-                setState(() {
-                  _tipIconShow = !_tipIconShow;
-                });
-              },
-            ),
-            Divider(
-              color: Colors.green,
-              thickness: 1,
-            ),
-            ListTile(
-              title: Text('Indicator show'),
-              trailing: Switch(
-                value: _tipIndicatorShow,
-                onChanged: (bool value) {
+              Divider(
+                color: Colors.green,
+                thickness: 1,
+              ),
+              ListTile(
+                title: Text('Indicator show'),
+                trailing: Switch(
+                  value: _tipIndicatorShow,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _tipIndicatorShow = value;
+                    });
+                  },
+                ),
+                onTap: () {
                   setState(() {
-                    _tipIndicatorShow = value;
+                    _tipIndicatorShow = !_tipIndicatorShow;
                   });
                 },
               ),
-              onTap: () {
-                setState(() {
-                  _tipIndicatorShow = !_tipIndicatorShow;
-                });
-              },
-            ),
-            Divider(
-              color: Colors.green,
-              thickness: 1,
-            ),
-            ListTile(
-              title: Text('IconPulse'),
-              trailing: Switch(
-                value: _tipIconPulse,
-                onChanged: (bool value) {
+              Divider(
+                color: Colors.green,
+                thickness: 1,
+              ),
+              ListTile(
+                title: Text('IconPulse'),
+                trailing: Switch(
+                  value: _tipIconPulse,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _tipIconPulse = value;
+                    });
+                  },
+                ),
+                onTap: () {
                   setState(() {
-                    _tipIconPulse = value;
+                    _tipIconPulse = !_tipIconPulse;
                   });
                 },
               ),
-              onTap: () {
-                setState(() {
-                  _tipIconPulse = !_tipIconPulse;
-                });
-              },
-            ),
-            Divider(
-              color: Colors.green,
-              thickness: 1,
-            ),
-            ListTile(
-              title: Text('ShowProgress'),
-              trailing: Switch(
-                value: _tipShowProgress,
-                onChanged: (bool value) {
+              Divider(
+                color: Colors.green,
+                thickness: 1,
+              ),
+              ListTile(
+                title: Text('ShowProgress'),
+                trailing: Switch(
+                  value: _tipShowProgress,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _tipShowProgress = value;
+                    });
+                  },
+                ),
+                onTap: () {
                   setState(() {
-                    _tipShowProgress = value;
+                    _tipShowProgress = !_tipShowProgress;
                   });
                 },
               ),
-              onTap: () {
-                setState(() {
-                  _tipShowProgress = !_tipShowProgress;
-                });
-              },
-            ),
-            Divider(
-              color: Colors.green,
-              thickness: 1,
-            ),
-          ],
+              Divider(
+                color: Colors.green,
+                thickness: 1,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
