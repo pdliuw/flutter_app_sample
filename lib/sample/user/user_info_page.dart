e17a226d58a4856afca4c1eb398f52e27d783340
Air <xkrossiapd163@163.com>
2020-02-05 21:23:37 +0800

    update: 'user info model'.

--- lib/sample/user/user_info_page.dart
+++ lib/sample/user/user_info_page.dart
@@ -1,6 +1,9 @@
 import 'dart:ui';
-
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
+
+import '../../common/helper/tip_helper.dart';
+import '../../common/helper/tip_type.dart';
 
 ///
 /// UserInfoPage
@@ -14,75 +17,120 @@ class UserInfoPage extends StatefulWidget {
 ///
 /// _UserInfoState
 class _UserInfoState extends State<UserInfoPage> {
-  double _sigmaX = 0;
-  double _sigmaY = 0;
+  String _phoneNumber = '12345678910';
+  String _personalGithub = 'https://github.com/pdliuw/flutter_app_sample';
+  String _personalSite = 'https://pdliuw.github.io/';
+  String _personalMailto = "xkrossiapd163@163.com";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
+        child: Stack(
           children: <Widget>[
-            Container(
-              child: Center(
-                child: Stack(
+            Column(
+              children: <Widget>[
+                Stack(
                   alignment: Alignment.center,
                   children: <Widget>[
-                    Image.network(
-                      "https://pdliuw.github.io/medias/avatar.jpg",
-                      fit: BoxFit.cover,
-                    ),
-                    BackdropFilter(
-                      filter: ImageFilter.blur(
-                        sigmaX: _sigmaX,
-                        sigmaY: _sigmaY,
-                      ),
+                    Banner(
+                      message: "air",
+                      color: Colors.green,
+                      location: BannerLocation.topEnd,
                       child: Container(
-                        width: 1,
-                        height: 1,
-                        color: Colors.green.withOpacity(0),
+                        child: Image.network(
+                          "https://pdliuw.github.io/medias/avatar.jpg",
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ],
                 ),
+                ExpansionTile(
+                  title: Row(
+                    children: <Widget>[
+                      Text("我的信息"),
+                    ],
+                  ),
+                  children: <Widget>[
+                    FlatButton.icon(
+                      textColor: Theme.of(context).primaryColor,
+                      onPressed: () {
+                        _launchURL(url: '$_personalGithub');
+                      },
+                      icon: Icon(Icons.web),
+                      label: Text('Github'),
+                    ),
+                    FlatButton.icon(
+                      textColor: Theme.of(context).primaryColor,
+                      onPressed: () {
+                        _launchMailto(mailto: '$_personalMailto');
+                      },
+                      icon: Icon(Icons.mail_outline),
+                      label: Text('Email'),
+                    ),
+                    FlatButton.icon(
+                      textColor: Theme.of(context).primaryColor,
+                      onPressed: () {
+                        _launchPhone(phone: '$_phoneNumber');
+                      },
+                      icon: Icon(Icons.call),
+                      label: Text('$_phoneNumber'),
+                    ),
+                    FlatButton.icon(
+                      textColor: Theme.of(context).primaryColor,
+                      onPressed: () {
+                        _launchSms(phone: '$_phoneNumber');
+                      },
+                      icon: Icon(Icons.sms),
+                      label: Text('$_phoneNumber'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: BackButton(
+                color: Theme.of(context).primaryColor,
               ),
             ),
-            Row(
-              children: <Widget>[
-                Slider(
-                  value: _sigmaX,
-                  onChanged: (double value) {
-                    setState(() {
-                      _sigmaX = value;
-                    });
-                  },
-                  label: "$_sigmaX",
-                  min: 0.0,
-                  max: 10.0,
-                  divisions: 10,
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Slider(
-                  value: _sigmaY,
-                  onChanged: (double value) {
-                    setState(() {
-                      _sigmaY = value;
-                    });
-                  },
-                  label: "$_sigmaY",
-                  min: 0.0,
-                  max: 10.0,
-                  divisions: 10,
-                ),
-              ],
-            ),
-            Text("0" * 1000),
-            Text("drawer"),
           ],
         ),
       ),
     );
+  }
+
+  _launchMailto({
+    String mailto = '12345678910',
+  }) async {
+    mailto = '$mailto';
+    await launch('mailto:$mailto');
+  }
+
+  _launchSms({
+    String phone = '12345678910',
+  }) async {
+    phone = '$phone';
+    await launch('sms:$phone');
+  }
+
+  _launchPhone({
+    String phone,
+  }) async {
+    await launch('tel:$phone');
+  }
+
+  _launchURL({
+    String url = 'https://flutter.dev',
+  }) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      TipHelper.showTip(
+        context: context,
+        tipType: TipType.ERROR,
+        message: 'Error url:$url',
+      );
+    }
   }
 }
