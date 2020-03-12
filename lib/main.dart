4ed9dda602099b48f8a4ee445f370ee5e48b0a82
Air <xkrossiapd163@163.com>
2020-03-12 12:38:59 +0800

    Add: Theme switch(Dart\Light)

--- lib/main.dart
+++ lib/main.dart
@@ -38,58 +38,62 @@ import 'sample/wheel/list_wheel_scroll_view_page.dart';
 @deprecated //2019
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
-void main() => runApp(
-      Airoute.createMaterialApp(
-        home: Scaffold(
-          body: LoginPage(),
-        ),
-        locale: const Locale('zh', 'CH'),
-        localizationsDelegates: [
-          ChineseCupertinoLocalizations.delegate,
-          DefaultCupertinoLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        supportedLocales: [
-          const Locale('zh', 'CH'),
-          const Locale('en', 'US'),
-        ],
-
-        ///全局静态路由的配置！
-        routes: <String, AirouteBuilder>{
-          "/LoginPage": () => LoginPage(),
-          "/MainPage": () => MainPage(),
-          "/TestPage": () => TestPage(),
-          "/ShoppingList": () => ShoppingListPage(),
-          "/MainSortListPage": () => MainSortListPage(),
-          "/ViewPagerFragmentPage": () => ViewPagerFragmentPage(),
-          "/CollapsingToolbarPage": () => CollapsingToolbarPage(),
-          "/MainAnimSortPage": () => MainAnimSortPage(),
-          "/AnimOfSwitchPage": () => AnimOfSwitchPage(),
-          "/AnimatedContainerPage": () => AnimatedContainerPage(),
-          "/OpacityAndAnimatedOpacityPage": () =>
-              OpacityAndAnimatedOpacityPage(),
-          "/FadeInImagePage": () => FadeInImagePage(),
-          "/HeroAnimPage": () => HeroAnimPage(),
-          "/TransformPage": () => TransformPage(),
-          "/AnimatedBuilderPage": () => AnimatedBuilderPage(),
-          "/ColorTweenPage": () => ColorTweenPage(),
-          "/AnappablePage": () => AnappablePage(),
-          "/CardMainPage": () => CardMainPage(),
-          "/CardInfoPage": () => CardInfoPage(),
-          "/DragListPage": () => DragListPage(),
-          "/DrawerVariouslyPage": () => DrawerVariouslyPage(),
-          "/FlushBarPage": () => FlushBarPage(),
-          "/DrawAnimPage": () => DrawAnimPage(),
-          "/ChartPage": () => ChartPage(),
-          "/UserInfoPage": () => UserInfoPage(),
-          "/MainPickerPage": () => MainPickerPage(),
-          "/FlutterWebPage": () => FlutterWebPage(),
-          "/AirLicensePage": () => AirLicensePage(),
-          "/ListWheelScrollViewPage": () => ListWheelScrollViewPage(),
-        },
+void main() {
+  runApp(
+    Airoute.createMaterialApp(
+      home: Scaffold(
+        body: LoginPage(),
       ),
-    );
+      themeMode: ThemeMode.system,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      locale: const Locale('zh', 'CH'),
+      localizationsDelegates: [
+        ChineseCupertinoLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('zh', 'CH'),
+        const Locale('en', 'US'),
+      ],
+
+      ///全局静态路由的配置！
+      routes: <String, AirouteBuilder>{
+        "/LoginPage": () => LoginPage(),
+        "/MainPage": () => MainPage(),
+        "/TestPage": () => TestPage(),
+        "/ShoppingList": () => ShoppingListPage(),
+        "/MainSortListPage": () => MainSortListPage(),
+        "/ViewPagerFragmentPage": () => ViewPagerFragmentPage(),
+        "/CollapsingToolbarPage": () => CollapsingToolbarPage(),
+        "/MainAnimSortPage": () => MainAnimSortPage(),
+        "/AnimOfSwitchPage": () => AnimOfSwitchPage(),
+        "/AnimatedContainerPage": () => AnimatedContainerPage(),
+        "/OpacityAndAnimatedOpacityPage": () => OpacityAndAnimatedOpacityPage(),
+        "/FadeInImagePage": () => FadeInImagePage(),
+        "/HeroAnimPage": () => HeroAnimPage(),
+        "/TransformPage": () => TransformPage(),
+        "/AnimatedBuilderPage": () => AnimatedBuilderPage(),
+        "/ColorTweenPage": () => ColorTweenPage(),
+        "/AnappablePage": () => AnappablePage(),
+        "/CardMainPage": () => CardMainPage(),
+        "/CardInfoPage": () => CardInfoPage(),
+        "/DragListPage": () => DragListPage(),
+        "/DrawerVariouslyPage": () => DrawerVariouslyPage(),
+        "/FlushBarPage": () => FlushBarPage(),
+        "/DrawAnimPage": () => DrawAnimPage(),
+        "/ChartPage": () => ChartPage(),
+        "/UserInfoPage": () => UserInfoPage(),
+        "/MainPickerPage": () => MainPickerPage(),
+        "/FlutterWebPage": () => FlutterWebPage(),
+        "/AirLicensePage": () => AirLicensePage(),
+        "/ListWheelScrollViewPage": () => ListWheelScrollViewPage(),
+      },
+    ),
+  );
+}
 
 class MyApp extends StatefulWidget {
   @override
