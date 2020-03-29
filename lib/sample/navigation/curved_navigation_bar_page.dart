6c9f21b5ccf2d88475362febab433af6b345d927
Air <xkrossiapd163@163.com>
2020-03-29 17:08:39 +0800

    Update: curved navigation style

--- lib/sample/navigation/curved_navigation_bar_page.dart
+++ lib/sample/navigation/curved_navigation_bar_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
 
 ///
 /// CurvedNavigationBarPage
@@ -19,95 +19,99 @@ class _CurvedNavigationBarState extends State<CurvedNavigationBarPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: CurvedNavigationBar(
-          key: _bottomNavigationKey,
-          index: 0,
-          height: 59.0,
-          items: <Widget>[
-            Container(
-              alignment: Alignment.topCenter,
-              padding: EdgeInsets.only(
-                bottom: 0,
-              ),
-              child: Column(
-                children: <Widget>[
-                  Icon(Icons.add, size: 20),
-                ],
-              ),
+      appBar: AppBar(
+        title: Text("Bottom"),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: 0,
+        height: 59.0,
+        items: <Widget>[
+          Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(
+              bottom: 0,
             ),
-            Container(
-              alignment: Alignment.topCenter,
-              padding: EdgeInsets.only(
-                bottom: 0,
-              ),
-              child: Column(
-                children: <Widget>[
-                  Icon(Icons.list, size: 20),
-                ],
-              ),
-            ),
-            Container(
-              alignment: Alignment.topCenter,
-              padding: EdgeInsets.only(
-                bottom: 0,
-              ),
-              child: Column(
-                children: <Widget>[
-                  Icon(Icons.compare_arrows, size: 20),
-                ],
-              ),
-            ),
-            Container(
-              alignment: Alignment.topCenter,
-              padding: EdgeInsets.only(
-                bottom: 0,
-              ),
-              child: Column(
-                children: <Widget>[
-                  Icon(Icons.call_split, size: 20),
-                ],
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.only(
-                bottom: 0,
-              ),
-              child: Column(
-                children: <Widget>[
-                  Icon(Icons.perm_identity, size: 20),
-                ],
-              ),
-            ),
-          ],
-          color: Colors.white,
-          buttonBackgroundColor: Colors.white,
-          backgroundColor: Colors.blueAccent,
-          animationCurve: Curves.easeInOut,
-          animationDuration: Duration(milliseconds: 600),
-          onTap: (index) {
-            setState(() {
-              _page = index;
-            });
-          },
-        ),
-        body: Container(
-          color: Colors.blueAccent,
-          child: Center(
             child: Column(
               children: <Widget>[
-                Text(_page.toString(), textScaleFactor: 10.0),
-                RaisedButton(
-                  child: Text('Go To Page of index 1'),
-                  onPressed: () {
-                    final CurvedNavigationBarState navBarState =
-                        _bottomNavigationKey.currentState;
-                    navBarState.setPage(1);
-                  },
-                )
+                Icon(Icons.add, size: 20),
               ],
             ),
           ),
-        ));
+          Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(
+              bottom: 0,
+            ),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.list, size: 20),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(
+              bottom: 0,
+            ),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.compare_arrows, size: 20),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(
+              bottom: 0,
+            ),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.call_split, size: 20),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(
+              bottom: 0,
+            ),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.perm_identity, size: 20),
+              ],
+            ),
+          ),
+        ],
+        color: Colors.white,
+        buttonBackgroundColor: Colors.white,
+        backgroundColor: Colors.blueAccent,
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 600),
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+      ),
+      body: Container(
+        color: Colors.blueAccent,
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(_page.toString(), textScaleFactor: 10.0),
+              RaisedButton(
+                child: Text('Go To Page of index 1'),
+                onPressed: () {
+                  final CurvedNavigationBarState navBarState =
+                      _bottomNavigationKey.currentState;
+                  navBarState.setPage(1);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
